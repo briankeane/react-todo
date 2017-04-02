f626afdebd170c1491ccd715402b6c06d7208aea
briankeane <lonesomewhistle@gmail.com>
2017-04-02 11:28:32 -0500

    started todoapp

--- app/app.jsx
+++ app/app.jsx
@@ -2,11 +2,13 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Router, Route, IndexRoute, hashHistory} = require('react-router');
 
+var ToDoApp = require('ToDoApp');
+
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <ToDoApp/>,
   document.getElementById('app')
 );
---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ToDoApp = () => {
+  return (
+    <h1>ToDoApp</h1>
+    )
+};
+
+
+module.exports = ToDoApp;
