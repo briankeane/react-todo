ceae86b47da806420c68bc933ed829d131417782
briankeane <lonesomewhistle@gmail.com>
2017-04-02 10:59:32 -0500

    initial commit

---
+++ app/tests/app.tests.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+
+
+describe('Tests', () => {
+  it ('should run tests', () => {
+    expect(true).toBe(true);
+  });
+});
