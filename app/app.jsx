826e5c55ce94131dbab92701379762e7ab4d7659
briankeane <lonesomewhistle@gmail.com>
2017-04-05 12:36:00 -0500

    test full paths in app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -3,10 +3,10 @@ var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var TodoApp = require('TodoApp');
+var TodoApp = require('./components/TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
+var TodoAPI = require('./api/TodoAPI');
 
 store.dispatch(actions.startAddTodos());
 
